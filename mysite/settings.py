3c12de9fcaf6d37a7ace47dd1618ba942bed3dbf
Haliyah Tijani <haliyya460@gmail.com>
2023-01-26 12:44:29 +0000

    added railway db

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,22 +84,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#  'default': {
-#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     'NAME': 'blogsite_db',
-#     'USER': 'postgres',
-#     'PASSWORD': 'haliyya460',
-#     'HOST':'localhost',
-#     'PORT': 5432,
-#     }
-# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 'default': {
+    'ENGINE': os.getenv("ENGINE"),
+    'NAME': os.getenv("NAME"),
+    'USER': os.getenv("USER"),
+    'PASSWORD': os.getenv("PASSWORD"),
+    'HOST':os.getenv("HOST"),
+    'PORT': os.getenv("PORT"),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
