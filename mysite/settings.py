63638c31c3f36fdbcb5ad551ee654d9676bd9f62
Haliyah Tijani <haliyya460@gmail.com>
2023-02-01 22:52:42 +0000

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,11 +171,11 @@ cloudinary.config(
     api_secret = os.getenv("CLOUDINARY_API_SECRET"),
 )
 
-CLOUDINARY_= {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY' : os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET' : os.getenv('CLOUDINARY_API_SECRET'),
-}
+# CLOUDINARY_= {
+#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY' : os.getenv('CLOUDINARY_API_KEY'),
+#     'API_SECRET' : os.getenv('CLOUDINARY_API_SECRET'),
+# }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 SITE_ID = 1
